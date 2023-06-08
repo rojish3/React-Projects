86e0da7324eca3799a5248165280e17ad56650f2
rojish3 <rojishranjit3@gmail.com>
2023-06-08 19:39:59 +0545

    UI changes

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -2,11 +2,38 @@ import React from 'react';
 import './App.css';
 const App = () => {
   return (
-    <div className='Calculator'>
-    <h1>Calculator</h1>
+    <div className='calc'>
+      <h1>Calculator</h1>
+      <input type='text' placeholder='0' id='answer' />
 
-    
+      <input type='button' value="C" className='button' />
+      <input type='button' value='/' className='button' />
+      <input type='button' value='*' className='button' />
+      <input type='button' value='-' className='button' />
 
+      <input type='button' value='7' className='button' />
+      <input type='button' value='8' className='button' />
+      <input type='button' value='9' className='button' />
+      <input type='button' value='+' className='button button1' />
+
+
+      <input type='button' value='4' className='button' />
+      <input type='button' value='5' className='button' />
+      <input type='button' value='6' className='button' />
+
+
+      <input type='button' value='1' className='button' />
+      <input type='button' value='2' className='button' />
+      <input type='button' value='3' className='button' />
+      <input type='button' value="=" className='button button1' />
+
+
+      <input type='button' value='0' className='button' />
+      <input type='button' value='.' className='button' />
+
+
+
+      
     </div>
   )
 }
