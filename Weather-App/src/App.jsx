6c251c8109ffb0f9a66d6a326d96ee36a99a3703
rojish3 <rojishranjit3@gmail.com>
2023-06-09 20:44:13 +0545

    Weather app

---
+++ Weather-App/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import './App.css';
+import axios from 'axios'
+
+
+const App = () => {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid={1584ff14e6e551b7bfef1023af262dc0}`;
+
+
+  return (
+    <div className='App'>
+
+    </div>
+  )
+}
+
+export default App
