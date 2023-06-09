3367f0dacef7185f034a11002e5c8df76e7bd872
rojish3 <rojishranjit3@gmail.com>
2023-06-09 09:50:03 +0545

    Project Completed

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 const App = () => {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.value));
+  }
+
+  const clear = () => {
+    setResult("");
+  }
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1));
+  }
+
+  const calculate = () => {
+    try{
+      setResult(eval(result).toString());
+    }catch(err){
+      setResult("Error")
+    }
+  }
   return (
     <div className='calc'>
-      <h1>Calculator</h1>
-      <input type='text' placeholder='0' id='answer' />
-
-      <input type='button' value="C" className='button' />
-      <input type='button' value='/' className='button' />
-      <input type='button' value='*' className='button' />
-      <input type='button' value='-' className='button' />
-
-      <input type='button' value='7' className='button' />
-      <input type='button' value='8' className='button' />
-      <input type='button' value='9' className='button' />
-      <input type='button' value='+' className='button button1' />
+      <input type='text' placeholder='0' id='answer' value={result} />
 
 
-      <input type='button' value='4' className='button' />
-      <input type='button' value='5' className='button' />
-      <input type='button' value='6' className='button' />
+      <input type='button' value="Clear" className='button long highlight' onClick={clear} />
+      <input type='button' value="C" className='button highlight' onClick={backspace} />
+      <input type='button' value='/' className='button highlight' onClick={handleClick} />
+
+      <input type='button' value='7' className='button' onClick={handleClick} />
+      <input type='button' value='8' className='button' onClick={handleClick} />
+      <input type='button' value='9' className='button' onClick={handleClick} />
+      <input type='button' value='*' className='button highlight' onClick={handleClick} />
+
+      <input type='button' value='4' className='button' onClick={handleClick} />
+      <input type='button' value='5' className='button' onClick={handleClick} />
+      <input type='button' value='6' className='button' onClick={handleClick} />
+      <input type='button' value='-' className='button highlight' onClick={handleClick} />
+
+      <input type='button' value='1' className='button' onClick={handleClick} />
+      <input type='button' value='2' className='button' onClick={handleClick} />
+      <input type='button' value='3' className='button' onClick={handleClick} />
+      <input type='button' value='+' className='button highlight' onClick={handleClick} />
+
+      <input type='button' value='0' className='button' onClick={handleClick} />
+      <input type='button' value='.' className='button' onClick={handleClick} />
+      <input type='button' value="=" className='button long highlight' onClick={calculate}/>
 
 
-      <input type='button' value='1' className='button' />
-      <input type='button' value='2' className='button' />
-      <input type='button' value='3' className='button' />
-      <input type='button' value="=" className='button button1' />
-
-
-      <input type='button' value='0' className='button' />
-      <input type='button' value='.' className='button' />
 
 
 
