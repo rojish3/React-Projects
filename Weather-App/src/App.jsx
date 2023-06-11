18a9b14f81ef65b90de8a4afe649bb901c975f4e
rojish3 <rojishranjit3@gmail.com>
2023-06-11 21:45:19 +0545

    time added

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -14,6 +14,9 @@ const App = () => {
           <div className='location'>
             <p>Kathmandu</p>
           </div>
+          <div className='time'>
+            <p>20:30</p>
+          </div>
           <div className='temp'>
             <h1>60F</h1>
           </div>
