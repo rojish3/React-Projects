1fb91dc81ead3081ac67f76e166c7f67cdbff06f
rojish3 <rojishranjit3@gmail.com>
2023-06-26 21:39:52 +0545

    contact page added

--- Ecommerce/src/Contact.js
+++ Ecommerce/src/Contact.js
@@ -37,7 +37,52 @@ const Contact = () => {
 
   return (
     <Wrapper>
-      <div>Contact</div>
+      <h2 className="common-heading">Contact Us</h2>
+      <iframe 
+        title="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d883.3144174031247!2d85.27684799567282!3d27.678429961205335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb180b7d99319d%3A0x9ac37c3956bf8034!2sManka%20gvahila%20dhuku!5e0!3m2!1sen!2snp!4v1687793135270!5m2!1sen!2snp" 
+        width="90%" 
+        height="450" 
+        style={{ border:0 }} 
+        allowFullScreen="" 
+        loading="lazy" 
+        referrerPolicy="no-referrer-when-downgrade"
+      >
+      </iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form className="contact-inputs" action="https://formspree.io/f/mqkvzqad" method="POST">
+            <input 
+              type="text" 
+              placeholder="username" 
+              name="username" 
+              autoComplete="off" 
+              required
+              // value=""
+            />
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+              // value=""
+            />
+            <textarea 
+              name="Message"
+              cols="30"
+              rows="10"
+              autoComplete="off"
+              placeholder="Enter your message"
+              required
+            ></textarea>
+            <input
+              type="submit"
+              value="Send"
+            />
+          </form>
+        </div>
+      </div>
     </Wrapper>
   );
 };
