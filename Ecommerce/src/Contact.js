f59c8ffb190625b663b3ba19a56577f3da38620e
rojish3 <rojishranjit3@gmail.com>
2023-06-26 23:11:41 +0545

    added error page

--- Ecommerce/src/Contact.js
+++ Ecommerce/src/Contact.js
@@ -1,40 +1,7 @@
 import styled from "styled-components";
 
 const Contact = () => {
-  const Wrapper = styled.section`
-    ${'' /* background-color: ${({theme}) => theme.colors.bg}; */}
-    height: 100vh;
-    padding: 9rem 0 5rem 0;
-    text-align: center;
-
-    .container {
-      margin-top: 6rem;
-
-      .contact-form {
-        max-width: 50rem;
-        margin: auto;
-
-        .contact-inputs {
-          display: flex;
-          flex-direction: column;
-          gap: 3rem;
-
-          input[type="submit"] {
-            cursor: pointer;
-            transition: all 0.2s;
-
-            &:hover {
-              background-color: ${({ theme }) => theme.colors.white};
-              border: 1px solid ${({ theme }) => theme.colors.btn};
-              color: ${({ theme }) => theme.colors.btn};
-              transform: scale(0.9);
-            }
-          }
-        }
-      }
-    }
-  `;
-
+  
   return (
     <Wrapper>
       <h2 className="common-heading">Contact Us</h2>
@@ -86,5 +53,36 @@ const Contact = () => {
     </Wrapper>
   );
 };
+    const Wrapper = styled.section`
+      padding: 9rem 0 5rem 0;
+      text-align: center;
+  
+      .container {
+        margin-top: 6rem;
+  
+        .contact-form {
+          max-width: 50rem;
+          margin: auto;
+  
+          .contact-inputs {
+            display: flex;
+            flex-direction: column;
+            gap: 3rem;
+  
+            input[type="submit"] {
+              cursor: pointer;
+              transition: all 0.2s;
+  
+              &:hover {
+                background-color: ${({ theme }) => theme.colors.white};
+                border: 1px solid ${({ theme }) => theme.colors.btn};
+                color: ${({ theme }) => theme.colors.btn};
+                transform: scale(0.9);
+              }
+            }
+          }
+        }
+      }
+    `;
 
 export default Contact;
