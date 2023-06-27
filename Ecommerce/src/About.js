d034503a448637ca89d74d27b5357df728c4b959
rojish3 <rojishranjit3@gmail.com>
2023-06-27 20:22:37 +0545

    Product page added

--- Ecommerce/src/About.js
+++ Ecommerce/src/About.js
@@ -1,14 +1,18 @@
 import React from 'react'
 // import styled from 'styled-components';
 import HeroSection from './components/HeroSection';
+import { useProductContext } from './context/productcontext';
 
 const About = () => {
+  const {myName} = useProductContext();
   const data = {
     name: 'Ecommerce Store',
   }
   return (
-
+    <>
+    {myName}
     <HeroSection heading={data} />
+    </>
     
   )
 }
