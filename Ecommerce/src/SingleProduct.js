5a22ee9d3a1c7f5891c82404364566d4527dc0f6
rojish3 <rojishranjit3@gmail.com>
2023-06-29 21:14:36 +0545

    single page context added

--- Ecommerce/src/SingleProduct.js
+++ Ecommerce/src/SingleProduct.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from "styled-components";
+import { useParams } from 'react-router-dom';
+import { useProductContext } from './context/productcontext';
 
+
+const API = "https://api.pujakaitem.com/api/products"
 
 const SingleProduct = () => {
+  const { getSingleProduct, isSingleLoading, singleProduct } = 
+    useProductContext();
+  const { id } = useParams();
+
+  const {
+    id: alias,
+    name,
+    company,
+    price,
+    description,
+    category,
+    stock,
+    stars,
+    reviews,
+  } = singleProduct;
+
+  useEffect(() => {
+    getSingleProduct(`${API}?id=${id}`);
+  }, []);
   return (
-    <Wrapper></Wrapper>
+    <Wrapper>single page {name}</Wrapper>
   )
 }
 
--- Ecommerce/src/context/productcontext.js
+++ Ecommerce/src/context/productcontext.js
@@ -42,6 +42,8 @@ const AppProvider = ({ children }) => {
     }
   };
 
+  
+
   useEffect(() => {
     getProducts(API);
   }, []);
