a89bbded79fa298dddf26719ab0603a449e29843
rojish3 <rojishranjit3@gmail.com>
2023-06-30 16:40:52 +0545

    singleproduct page completed

--- Ecommerce/src/SingleProduct.js
+++ Ecommerce/src/SingleProduct.js
@@ -9,6 +9,7 @@ import FormatPrice from './helpers/FormatPrice';
 import { TbTruckDelivery, TbReplace } from 'react-icons/tb';
 import { MdSecurity } from 'react-icons/md'
 import Star from './components/Star';
+import AddToCart from './components/AddToCart';
 
 
 const API = "https://api.pujakaitem.com/api/products"
@@ -94,9 +95,11 @@ const SingleProduct = () => {
               <span> { company }</span>
             </p>
           </div>
+          <hr />
+          {stock  > 0 && <AddToCart product={singleProduct} />}
           </div>
         </div>
-        </Container>
+      </Container>
     </Wrapper>
   )
 }
