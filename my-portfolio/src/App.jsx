aa58c5d1769db178f14e46379d79254d9382e690
rojish3 <rojishranjit3@gmail.com>
2023-07-22 20:15:16 +0545

    hero page added

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
 
---
+++ my-portfolio/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <>
+        <h2>Hi, I am Rojish Ranjit</h2>
+        <p>I am a front end web developer</p>
+    </>
+  )
+}
+
+export default Hero
