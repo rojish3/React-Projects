f8e1b218b7d239721ec637c3f6fa5964e32385b4
rojish3 <rojishranjit3@gmail.com>
2023-07-27 21:08:50 +0545

    profile pic added

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -1,9 +1,12 @@
+import ProfilePicture from '../assets/img.jpg';
 const Hero = () => {
   return (
-    <>
-        <h2>Hi, I am Rojish Ranjit</h2>
-        <p>I am a front end web developer</p>
-    </>
+    <div className="container bg-black m-auto text-white flex">
+      <img src={ProfilePicture} className="rounded-3xl h-1/3 w-1/3 flex align-middle justify-center" />
+        <h2 className=''>Hi, I am Rojish Ranjit.</h2>
+        <br />
+        <h3>I am a front end web developer</h3>
+    </div>
   )
 }
 
