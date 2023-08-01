4a8d4ea20f99059bf241b5f2b33de926ac662091
rojish3 <rojishranjit3@gmail.com>
2023-08-01 21:18:20 +0545

    project card added

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Hero />
       <Skills />
+      <Projects />
     </>
   )
 }
--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,6 +1,14 @@
+import ecommerce from '../assets/ecommerce.png';
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="projects m-10 p-10">
+      <h1 className="text-4xl mb-5">Projects</h1>
+      <div className="card h-72 w-96 p-3 bg-gray-300 shadow-xl">
+        <img src={ecommerce} alt="ecommerce"></img>
+        <h3 className="text-lg text-center my-2 font-bold">Gadget Store (E-commerce App)</h3>
+        <p>Gadget Store is an one stop destination for your electornic gadget needs.</p>
+      </div>
+    </div>
   )
 }
 
