f66e62ad3f152eaece8560496f57c63eb2fce043
rojish3 <rojishranjit3@gmail.com>
2023-08-01 21:27:43 +0545

    responsive design

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,15 +1,33 @@
-import ecommerce from '../assets/ecommerce.png';
+import ecommerce from "../assets/ecommerce.png";
 const Projects = () => {
   return (
     <div className="projects m-10 p-10">
       <h1 className="text-4xl mb-5">Projects</h1>
-      <div className="card h-72 w-96 p-3 bg-gray-300 shadow-xl">
-        <img src={ecommerce} alt="ecommerce"></img>
-        <h3 className="text-lg text-center my-2 font-bold">Gadget Store (E-commerce App)</h3>
-        <p>Gadget Store is an one stop destination for your electornic gadget needs.</p>
+      <div className="cards flex space-x-12">
+        <div className="card h-72 w-96 p-3 bg-gray-300 shadow-xl overflow-hidden">
+          <img src={ecommerce} alt="ecommerce"></img>
+          <h3 className="text-lg text-center my-2 font-bold">
+            Gadget Store (E-commerce App)
+          </h3>
+          <p>
+            Gadget Store is an one stop destination for your electornic gadget
+            needs.
+          </p>
+        </div>
+
+        <div className="card h-72 w-96 p-3 bg-gray-300 shadow-xl overflow-hidden">
+          <img src={ecommerce} alt="ecommerce"></img>
+          <h3 className="text-lg text-center my-2 font-bold">
+            Gadget Store (E-commerce App)
+          </h3>
+          <p>
+            Gadget Store is an one stop destination for your electornic gadget
+            needs.
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
