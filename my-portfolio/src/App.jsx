3360a664cb7e3356d728c181b88149e90c8f6dbd
rojish3 <rojishranjit3@gmail.com>
2023-08-01 20:58:27 +0545

    modification on skills section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Skills />
     </>
   )
 }
